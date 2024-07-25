2e07fd1e2eea1475cfdbb1e0fd78c032b9d9a7a5
SUJP123 <sujaypatel04@gmail.com>
2024-07-25 00:53:13 -0400

    first commit

---
+++ src/main/java/com/collegeproject/dealsforyou/DealsForYouApplication.java
@@ -0,0 +1,13 @@
+package com.collegeproject.dealsforyou;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DealsForYouApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DealsForYouApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/collegeproject/dealsforyou/DealsForYouApplicationTests.java
@@ -0,0 +1,13 @@
+package com.collegeproject.dealsforyou;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DealsForYouApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
